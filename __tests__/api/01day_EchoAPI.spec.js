59cd84a3d23e78d65e8ef2787bedd00c1b4b52b0
Irina Ostrovskaya <ostrovskaya.i.k@gmail.com>
2022-04-03 01:16:19 +0300

    01 day tests added

---
+++ __tests__/api/01day_EchoAPI.spec.js
@@ -0,0 +1,34 @@
+const frisby = require('frisby');
+const Joi = frisby.Joi;
+describe('Postman APIs testing: Day 04', () => {
+  
+  it('Post data with Postman Echo API', () => {
+    return frisby.post('https://postman-echo.com/post', {
+      payload: 'hello world!',
+    })
+    .expect('status', 200)
+    .expect('json', 'data', {
+      payload: 'hello world!'
+    })
+    .then((result) => {
+      console.log(result.json);
+      })
+  });
+
+  it('Json body response fields check', () => {
+    return frisby.post('https://postman-echo.com/post')
+    .expect('json', 'args',{})
+    .expect('json', 'data',{})
+    .expect('json', 'files',{})
+    .expect('json', 'form',{})
+    .expect('jsonTypes', 'headers', Joi.object())
+  });
+
+});
+
+//prevent console from closure
+require('readline')
+.createInterface(process.stdin, process.stdout)
+.question("Press [Enter] to exit...", function () {
+  process.exit();
+});
