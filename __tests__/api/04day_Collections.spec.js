1ef0ca20c1c491aca6270f749746061f3499b0f5
Irina Ostrovskaya <ostrovskaya.i.k@gmail.com>
2022-04-02 21:03:19 +0300

    13 day tests finished, 04 day added

---
+++ __tests__/api/04day_Collections.spec.js
@@ -0,0 +1,20 @@
+const frisby = require('frisby');
+const Joi = frisby.Joi;
+describe('Postman APIs testing: Day 04', () => {
+  it('Auth with API-key Status 200 response', () => {
+    return frisby.get('https://api.getpostman.com/collections')
+      .expect('status', 200);
+  });
+  
+  it('Response contain collections', () => {
+    return frisby.get('https://api.getpostman.com/collections')
+      
+  });
+});
+
+//prevent console from closure
+require('readline')
+.createInterface(process.stdin, process.stdout)
+.question("Press [Enter] to exit...", function () {
+  process.exit();
+});
