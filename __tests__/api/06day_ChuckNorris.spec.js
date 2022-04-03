8dd459b3b9ed25d1f7e2ac7b9afd0cf1f2b2a981
Irina Ostrovskaya <ostrovskaya.i.k@gmail.com>
2022-04-03 14:26:06 +0300

    06 day tests created

--- __tests__/api/06day_ChuckNorris.spec.js
+++ __tests__/api/06day_ChuckNorris.spec.js
@@ -5,13 +5,33 @@ describe('Postman APIs testing: Day 06', () => {
   
   it('API Status 200 response', () => {
     return frisby.get('https://api.chucknorris.io/')
-    .expect('status', 200)
-    .then((result) => {
-      console.log(result.body);
-    });
+    .expect('status', 200);
   });
 
- 
+  it('Correct body response types', () => {
+    return frisby.get('https://api.chucknorris.io/jokes/random')
+    .expect('status', 200)
+    .then((result) => {
+      console.log(result.json)
+    })
+    .expect('jsonTypes','categories', Joi.array())
+    .expect('jsonTypes','created_at', Joi.string())
+    .expect('jsonTypes','icon_url', Joi.string().uri())
+    .expect('jsonTypes','id', Joi.string())
+    .expect('jsonTypes','updated_at', Joi.string())
+    .expect('jsonTypes','url', Joi.string().uri())
+    .expect('jsonTypes','value', Joi.string())
+  });
+
+  it('Get random joke', () => {
+    return frisby.get('https://api.chucknorris.io/jokes/random')
+    .expect('status', 200)
+    .then((result) => {
+      console.log(result.json);
+      const joke = result.json.value;
+      console.log(joke);
+    })
+  });
 
 });
 
