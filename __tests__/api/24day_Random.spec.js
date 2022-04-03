4bc693b6dd8ee11bde3c27be8b9a5dfd9c2c6853
Irina Ostrovskaya <ostrovskaya.i.k@gmail.com>
2022-04-03 22:00:24 +0300

    24 day tests added

--- __tests__/api/24day_Random.spec.js
+++ __tests__/api/24day_Random.spec.js
@@ -15,6 +15,40 @@ describe('Postman APIs testing: Day 24', () => {
     });
   });
 
+  it('Moments in time 200 response', () => {
+    return frisby.get('http://worldtimeapi.org/api/ip')
+    .expect('status', 200)
+    .expect((result) => {
+      console.log(result.json)
+    });
+  });
+
+  it('Moments in time correct data types', () => {
+    return frisby.get('http://worldtimeapi.org/api/ip')
+    .expect('status', 200)
+    .expect('jsonTypes','abbreviation', Joi.string())
+    .expect('jsonTypes','client_ip', Joi.string())
+    .expect('jsonTypes','datetime', Joi.string())
+    .expect('jsonTypes','day_of_week', Joi.number())
+    .expect('jsonTypes','day_of_year', Joi.number())
+    .expect('jsonTypes','dst', Joi.boolean())
+    .expect('jsonTypes','dst_from', [
+      null,
+      Joi.number(),
+    ])
+    .expect('jsonTypes','dst_offset', Joi.number())
+    .expect('jsonTypes','dst_until', [
+      null,
+      Joi.number(),
+    ])
+    .expect('jsonTypes','raw_offset', Joi.number())
+    .expect('jsonTypes','timezone', Joi.string())
+    .expect('jsonTypes','unixtime', Joi.number())
+    .expect('jsonTypes','utc_datetime', Joi.string())
+    .expect('jsonTypes','utc_offset', Joi.string())
+    .expect('jsonTypes','week_number', Joi.number())
+  });
+
 });
 
 //prevent console from closure
