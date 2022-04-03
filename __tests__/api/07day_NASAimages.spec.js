8c51ab99d5178d0f3b0c5bd97889d6bf8c42515b
Irina Ostrovskaya <ostrovskaya.i.k@gmail.com>
2022-04-03 14:48:31 +0300

    07 day tests added

---
+++ __tests__/api/07day_NASAimages.spec.js
@@ -0,0 +1,79 @@
+const frisby = require('frisby');
+const Joi = frisby.Joi;
+
+describe('Postman APIs testing: Day 07', () => {
+  
+  it('API Status 404 response if not specificators', () => {
+    return frisby.get('https://api.nasa.gov/planetary')
+    .expect('status', 404);
+  });
+
+  it('API Status 403 response if no api key provided', () => {
+    return frisby.get('https://api.nasa.gov/planetary/apod')
+    .expect('status', 403);
+  });
+
+  it('API Status 200 response', () => {
+    return frisby.fetch('https://api.nasa.gov/planetary/apod', {
+      method: 'get',
+      headers: {
+        Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'x-api-key': 'DEMO_KEY',
+
+      },
+    })
+    .expect('status', 200)
+    .then((result) => {
+      console.log(result.json);
+    });
+  });
+
+  it('Check types of info in response body', () => {
+    return frisby.fetch('https://api.nasa.gov/planetary/apod', {
+        method: 'get',
+        headers: {
+          Accept: 'application/json',
+                  'Content-Type': 'application/json',
+                  'x-api-key': 'DEMO_KEY',
+        },
+      })
+    .expect('status', 200)
+    .expect('jsonTypes', {
+      copyright: Joi.string(),
+      date: Joi.string(),
+      explanation: Joi.string(),
+      hdurl: Joi.string().uri(),
+      media_type: Joi.string(),
+      service_version: Joi.string(),
+      title: Joi.string(),
+      url: Joi.string().uri(),
+    });
+  });
+
+  it('Get different images number', () => {
+    const params = new URLSearchParams({ count: 2 });
+    const key = new URLSearchParams({ api_key: 'DEMO_KEY' });
+    return frisby.fetch('https://api.nasa.gov/planetary/apod' + '?' + key + '&' + params, {
+      method: 'get',
+      headers: {
+        Accept: 'application/json',
+                'Content-Type': 'application/json',
+      },
+    })
+    .expect('status', 200)
+    .then((result) => {
+      console.log(result.json);
+    });
+  });
+
+  
+
+});
+
+//prevent console from closure
+require('readline')
+.createInterface(process.stdin, process.stdout)
+.question("Press [Enter] to exit...", function () {
+  process.exit();
+});
