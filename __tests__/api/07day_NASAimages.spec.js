556670aaa02a6794d31c511e12ce127feffd9e7d
Irina Ostrovskaya <ostrovskaya.i.k@gmail.com>
2022-04-03 15:01:11 +0300

    07 day tests added debugging task from postman challenge

--- __tests__/api/07day_NASAimages.spec.js
+++ __tests__/api/07day_NASAimages.spec.js
@@ -67,7 +67,32 @@ describe('Postman APIs testing: Day 07', () => {
     });
   });
 
-  
+  it('Debugging task in postman: get all titles and url of images', () => {
+    const temp_count = 3; 
+    const params = new URLSearchParams({ count: temp_count });
+    const key = new URLSearchParams({ api_key: 'DEMO_KEY' });
+    return frisby.fetch('https://api.nasa.gov/planetary/apod' + '?' + key + '&' + params, {
+      method: 'get',
+      headers: {
+        Accept: 'application/json',
+                'Content-Type': 'application/json',
+      },
+    })
+    .expect('status', 200)
+    .then((result) => {
+      console.log(result.json);
+    })
+    .then(function (result) {
+      const count = temp_count;
+      for (let index = 0; index < count; index++) {
+        const imagetitle = result.json[index].title;
+        const imageurl = result.json[index].url;
+        console.log('Image ' + index+1 + ': ' + '\n'
+        + '   ' + 'Title: ' + imagetitle + '\n'
+        + '   ' + 'URL: ' + imageurl + '\n')
+      }
+    })
+  });
 
 });
 
