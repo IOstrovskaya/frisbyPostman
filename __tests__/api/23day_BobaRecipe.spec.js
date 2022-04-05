df77fb59e3edbab8f91902e816197ecd5afcd41f
Irina Ostrovskaya <ostrovskaya.i.k@gmail.com>
2022-04-05 14:47:55 +0300

    23 day tests added

--- __tests__/api/23day_BobaRecipe.spec.js
+++ __tests__/api/23day_BobaRecipe.spec.js
@@ -1,5 +1,7 @@
 const frisby = require('frisby');
 const Joi = frisby.Joi;
+const fs = require('fs');
+
 
 describe('Postman APIs testing: Day 23', () => {
 
@@ -12,6 +14,24 @@ describe('Postman APIs testing: Day 23', () => {
     .expect((result) => {
       console.log(result.json)
     });
+  });
+
+  jest.setTimeout(100000);
+  it('Check for boba in Region with data file upload', async() => {
+    const fileData = fs.readFileSync('D:/Documents/Git/frisbyPostman/__tests__/test_files_data/geoMap.csv', 'utf8');
+    const rows = fileData.split('\n');
+    console.log(rows);
+    for (let index = 1; index < rows.length; index++) {
+      const values = rows[index].split(',');
+      const region = values[0];
+      const boba=values[1];
+      const result = await frisby.get('https://postman-echo.com/get?' + region + '=' + boba);
+      console.log(result.json);
+      if(result.json.args[region] === '62') {
+        console.log('Region with 62% boba popularity is fould');
+        return;
+      }  
+    }
   });
 
 });
